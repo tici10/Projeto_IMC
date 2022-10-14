6a96fc9f90a8a1b0d969e77faae2656f7e6a8e71
tici10 <114961257+tici10@users.noreply.github.com>
2022-10-14 16:14:22 -0300

    Rename SQL.Exercicios.sql to Banco_de_dados/SQL.Exercicios.sql

--- Banco_de_dados/SQL.Exercicios.sql
+++ Banco_de_dados/SQL.Exercicios.sql
@@ -2,14 +2,14 @@ create table exercicios(
 CodE int identity (1,1),
 CodLogin int,
 tipo_execicio varchar (100) not null,
-repetições int,
+repetiÃ§Ãµes int,
 series int,
 min_descanso varchar(50),
 primary key (CodE),
 foreign key (CodLogin) references login(CodLogin)
 )
 
-insert into exercicios(tipo_execicio, repetições, series, min_descanso)
+insert into exercicios(tipo_execicio, repetiÃ§Ãµes, series, min_descanso)
 values ('Agachemento', 10, 5,'10s'),
 ('Abdominal',10, 5, '10s'),
 ('Abdominal prancha', 5, 2, '15s');
